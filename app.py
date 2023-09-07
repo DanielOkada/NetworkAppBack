3d876a8bc46cae25e2498cd2678815429ec1c254
DanielOkada <s223298@stn.nagaokaut.ac.jp>
2023-09-08 05:50:29 +0900

    セッションを使わないように変更 古いコードを削除

--- app.py
+++ app.py
@@ -1,85 +1,30 @@
 from flask import *
 import pandas as pd
 import network
-import secrets
-import os
 
 app = Flask(__name__)
-
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @app.route('/')
 def index():
-    if "user_key" not in session:
-        session["user_key"] = secrets.token_hex()
-    return render_template("index.html", upload=url_for("upload"), test=url_for("get_file_name"))
-
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    if "user_key" not in session:
-        session["user_key"] = secrets.token_hex()
-    file = request.files['file']
-
-    session["filename"] = file.filename
-    print(file.filename)
-    file_path = f'uploads/{session["user_key"]}/'
-    os.makedirs(file_path, exist_ok=True)
-    file.save(file_path + session["filename"])
-
-    session["file_path"] = file_path
-
-    return {"status": "success"}
+    return render_template("index.html")
 
 
 @app.route('/get_network')
 def get_network():
-    input_book = pd.ExcelFile(session["file_path"] + session["filename"])
-    json = network.get_network_image(input_book, session["sheet"])
-    # print(json)
-    return json
+    return "未実装"
 
 
-@app.route('/get_networks')
-def get_networks():
-    input_book = pd.ExcelFile(session["file_path"] + session["filename"])
-
-    networks = {}
-    for sheet in input_book.sheet_names:
-        networks[sheet] = network.get_network_cyto(input_book, sheet)
-
-    return networks
-
-
-@app.route('/get_networks_d3')
+@app.route('/get_networks_d3', methods=["POST"])
 def get_networks_d3():
-    input_book = pd.ExcelFile(session["file_path"] + session["filename"])
+    file = request.files["file"]
+    input_book = pd.ExcelFile(file)
 
     networks = {}
     for sheet in input_book.sheet_names:
         networks[sheet] = network.get_network_d3(input_book, sheet)
 
     return networks
-
-
-@app.route('/get_file_name')
-def get_file_name():
-    return session["filename"]
-
-
-@app.route('/get_sheets')
-def get_sheets():
-    print(session["file_path"] + session["filename"])
-    input_book = pd.ExcelFile(session["file_path"] + session["filename"])
-    return {"sheets": input_book.sheet_names}
-
-
-@app.route('/set_sheet', methods=["POST"])
-def set_sheet():
-    session["sheet"] = request.form["sheet"]
-    print(session["sheet"])
-    return {"status": "success"}
 
 
 @app.route('/get_saidai_renketsu', methods=["POST"])
